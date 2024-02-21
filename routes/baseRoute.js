abc22d69b850b2f5b0eb9ec07851ff19f9b35b37
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-02-21 07:37:36 +0100

    Update baseRoute.js

--- routes/baseRoute.js
+++ routes/baseRoute.js
@@ -2,13 +2,21 @@ const express = require("express")
 const router = new express.Router() 
 const baseController = require("../controllers/baseController")
 const utilities = require("../utilities/")
-const regValidate = require('../utilities/add-validation')
+const validate = require("../utilities/comment-validation")
 
 router.get("/", utilities.checkJWTToken, utilities.handleErrors(baseController.buildHome));
-router.get("/post-comment", utilities.handleErrors(baseController.buildPostComment))
-router.post("/post-comment", utilities.checkJWTToken, utilities.handleErrors(baseController.postComment));
+router.get("/post-comment", utilities.checkJWTToken, utilities.handleErrors(baseController.buildPostComment))
+router.post("/post-comment", 
+    utilities.checkJWTToken, 
+    validate.postCommentRules(),
+    validate.checkPostData,
+    utilities.handleErrors(baseController.postComment));
 router.get("/comment", utilities.handleErrors(baseController.buildComment));
-router.post("/delete-comment", utilities.checkJWTToken, utilities.handleErrors(baseController.deleteComment));
-router.get("/delete-comment", utilities.handleErrors(baseController.buildDeleteComment))
+router.post("/delete-comment",
+    utilities.checkJWTToken,
+    validate.deleteCommentRules(),
+    validate.checkDeleteData,
+    utilities.handleErrors(baseController.deleteComment));
+router.get("/delete-comment", utilities.checkJWTToken, utilities.handleErrors(baseController.buildDeleteComment))
 
 module.exports = router;
